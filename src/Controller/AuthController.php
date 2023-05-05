f9713d248d363a27f65e18c2488003c5e8b00a9f
feriale-bourega <92152060+feriale-bourega@users.noreply.github.com>
2023-05-05 13:13:54 +0200

    AuthController pour la connexion

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -20,6 +20,24 @@ class AuthController
         }
 
     }
+
+    public function connController($email, $password)
+    {
+        $UserModel = new ModelUser();
+
+        $check_conn = $UserModel->rowCountUser($email);
+        if($check_conn === 1)
+        {
+
+            $user = $UserModel->getUser($email);
+
+            if(password_verify($password, $user['password']))
+
+            if (session_status() == PHP_SESSION_NONE) {session_start();}
+            $_SESSION['user'] = ['id' => $user['id'], 'first_name' => $user['first_name'], 'last_name' => $user['last_name']];
+        }
+    }
+
 }
 
 ?>
