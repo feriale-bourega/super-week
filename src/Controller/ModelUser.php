9fa510da98820db4f6696cb56c00efb0a8ffac54
feriale-bourega <92152060+feriale-bourega@users.noreply.github.com>
2023-05-05 12:20:11 +0200

    Controller pour la liste des utilisateurs

---
+++ src/Controller/ModelUser.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Model;
+use PDO;
+class ModelUser
+{
+    private ?PDO $conn;
+
+    public function getConn() {
+        try {
+            return $conn = new PDO('mysql:host=localhost;dbname=super_week', 'root', '');
+        } catch (\PDOException $e) {
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    public function insertDB(?string $table,?array $values)
+    {
+        $req = $this->getConn()->prepare("INSERT INTO $table () VALUES () ");
+
+
+    }
+
+    public function fakerUserDB($firstName, $lastName, $email, $password)
+    {
+
+        $req = $this->getConn()->prepare("INSERT INTO user (email, first_name, last_name, password) VALUES (:email, :first_name, :last_name, :password)");
+        $req->execute([
+            ":email" => $email,
+            ":first_name" => $firstName,
+            ":last_name" => $lastName,
+            ":password" => password_hash($password, PASSWORD_DEFAULT)
+        ]);
+    }
+
+    public function getUserDB() {
+        $req = $this->getConn()->prepare("SELECT * FROM user");
+        $req->execute([]);
+        return $req->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+?>
